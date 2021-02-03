4cadfcb02f21371277adf224d5fed4a2375bfc4a
Minke Zhang <minke.zhang@gmail.com>
2021-02-03 02:21:17 -0800

    Add plugins directory

---
+++ _plugins/detached_front_matter.rb
@@ -0,0 +1,26 @@
+# Adds support for detached front matter.
+# See https://github.com/jekyll/jekyll/issues/1082#issuecomment-177167222.
+
+module ReadMetaFiles
+  class Generator < Jekyll::Generator
+    def generate(site)
+
+      site.posts.docs.each do |doc|
+        meta_file = (doc.path + ".yml")
+        if File.exist?(meta_file)
+            meta_data = SafeYAML.load_file(meta_file)
+            meta_data.each { |key, value| doc.data[key] = value}
+        end
+      end
+
+      site.pages.each do |doc|
+        meta_file = (doc.path + ".yml")
+        if File.exist?(meta_file)
+            meta_data = SafeYAML.load_file(meta_file)
+            meta_data.each { |key, value| doc.data[key] = value}
+        end
+      end
+
+    end
+  end
+end
